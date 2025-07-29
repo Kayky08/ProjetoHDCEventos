aa3e99025a7715c8bf7716512f26f19c1e4521c1
kayky.silva <kayky.silva@01NOT0053TI>
2025-07-29 16:51:02 -0300

    Terminando a parte de Migrations e partindo para os Models

--- database/migrations/2025_07_29_184003_create_products_table.php
+++ database/migrations/2025_07_29_184003_create_products_table.php
@@ -14,9 +14,23 @@ return new class extends Migration
     /*
       Migrations servem como um versionamento do banco de dados, alem de facilitarem as ações com o mesmo
       
-      para criar: php artisan make:migration nome_da_migration (EX: create_produtc_table)
+      para criar: php artisan make:migration nome_da_migration
+        Exemplos:
+
+        Criação da tabela: create_[tabela]_table
+        Adição de coluna(s): add_[coluna]to[tabela]_table
+        Remoção de coluna(s): remove_[coluna]from[tabela]_table
+        Renomear tabela: rename_[tabela]to[nova]_table
+        Alterar tipo de coluna: change_[coluna]on[tabela]_table
+        Adicionar chave estrangeira: add_foreign_key_to_[tabela]_table
+        Criar tabelas pivô (N:N): create_[tabela_pivo]_table
+
+      para rodar a migratio: php artisan migrate
       para verificar o status: php artisan migrate:status
       para recriar as migrations: php artisan migrate:fresh (CUIDADO: Ele recria as tabelas sem os dados!!!)
+      para voltar uma migration: php artisan migrate:rollback
+      para voltar todas as migrations: php artisan migrate:reset
+      para voltar todas as migrations e rodar novamente: php artisan migrate:refresh
     */
     public function up(): void
     {
