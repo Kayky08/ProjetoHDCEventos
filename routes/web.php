a33546c9859bf0b837b20091ef99aa926fd53c31
kayky.silva <kayky.silva@01NOT0053TI>
2025-07-30 16:29:08 -0300

    Alterando comentários

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,28 @@ Route::post('/eventos', [EventController::class, 'store']);//
  *   2. Verificar se o antivirus não esta barrando
  *   3. Verificar se tem o banco de dados
  *   4. Verificar se criou uma chave de acesso: php artisan key:generate
- */
-
- /*
-    Migrations servem como um versionamento do banco de dados, alem de facilitarem as ações com o mesmo
-    
-    para criar: php artisan make:migration nome_da_migration
-    Exemplos:
-
-    Criação da tabela: create_[tabela]_table
-    Adição de coluna(s): add_[coluna]to[tabela]_table
-    Remoção de coluna(s): remove_[coluna]from[tabela]_table
-    Renomear tabela: rename_[tabela]to[nova]_table
-    Alterar tipo de coluna: change_[coluna]on[tabela]_table
-    Adicionar chave estrangeira: add_foreign_key_to_[tabela]_table
-    Criar tabelas pivô (N:N): create_[tabela_pivo]_table
-
-    para rodar a migratio: php artisan migrate
-    para verificar o status: php artisan migrate:status
-    para recriar as migrations: php artisan migrate:fresh (CUIDADO: Ele recria as tabelas sem os dados!!!)
-    para voltar uma migration: php artisan migrate:rollback
-    para voltar todas as migrations: php artisan migrate:reset
-    para voltar todas as migrations e rodar novamente: php artisan migrate:refresh
+ *
+ * MIGRATIONS: 
+ * 
+ *   Definição: servem como um versionamento do banco de dados, alem de facilitarem as ações com o mesmo
+ * 
+ *   Para criar: php artisan make:migration nome_da_migration
+ *   E existem varios tipos como nos exemplos:
+ * 
+ *   Criação da tabela: create_[tabela]_table
+ *   Adição de coluna(s): add_[coluna]to[tabela]_table
+ *   Remoção de coluna(s): remove_[coluna]from[tabela]_table
+ *   Renomear tabela: rename_[tabela]to[nova]_table
+ *   Alterar tipo de coluna: change_[coluna]on[tabela]_table
+ *   Adicionar chave estrangeira: add_foreign_key_to_[tabela]_table
+ *   Criar tabelas pivô (N:N): create_[tabela_pivo]_table
+ *  
+ *   Comandos para serem executados no artisan:
+ * 
+ *   para rodar a migration: php artisan migrate
+ *   para verificar o status: php artisan migrate:status
+ *   para recriar as migrations: php artisan migrate:fresh (CUIDADO: Ele recria as tabelas sem os dados!!!)
+ *   para voltar uma migration: php artisan migrate:rollback
+ *   para voltar todas as migrations: php artisan migrate:reset
+ *   para voltar todas as migrations e rodar novamente: php artisan migrate:refresh
 */
